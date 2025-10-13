744677c95697ef9272532f879a1bc3e0e601ff03
Evgeniy Nochevnov <accounts@nochevnov.ru>
2025-10-13 12:25:01 +0300

    Remove obsolete code

--- config/initializers/active_model.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "../../lib/types"
-require_relative "../../lib/types/non_serializable"
-
-ActiveModel::Type.register(:non_serializable, Types::NonSerializable)
--- lib/types.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Empty here, because we load this file before Zeitwerk.
-module Types
-end
